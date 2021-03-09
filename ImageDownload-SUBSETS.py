756f5c1b334b63e7d57b77ae94d1d8fcebd4bf18
bimissing <39038771+bimissing@users.noreply.github.com>
2021-03-10 01:20:30 +0900

    Update ImageDownload-SUBSETS.py

--- ImageDownload-SUBSETS.py
+++ ImageDownload-SUBSETS.py
@@ -11,62 +11,6 @@ import shutil, os
 import random
 from shutil import copy2
 
-#Read 1-DimensionFeature CSV file
-# with open('/Users/admin/PycharmProjects/fileProcessing/1-DimensionFinalNew1origin.csv','r') as csvFile:
-#     reader = csv.reader(csvFile)
-#     result = [row for row in reader]
-
-#Read JSON file
-with open('./recipeData.json','r') as f:
-  data = json.load(f)
-
-
-#Convert Dict to pdframe
-pdData = pd.DataFrame(data.items(), columns=['recipeID', 'Value'])
-#Convert pdframe to list
-listDict = []
-for index, row in pdData.iterrows():
-    listDict.append(row["Value"])
-
-FileNameList = []
-imgLinkList = []
-
-for item in listDict:
-  #Obtain recipe ID
-  recipeID = item['recipeID']
-  # Step List
-  instructionsDict = item['instructions']
-
-  stepNum = 0
-  for i in instructionsDict:
-    #Download Images
-    try:
-        FileName = recipeID + "_" + str(stepNum)
-        FileNameList.append(FileName)
-
-        imgLink = i['imgLink']
-        imgLinkList.append(imgLink)
-
-        stepNum = stepNum + 1
-    except:
-        continue
-
-LinkFileList = list(zip(FileNameList, imgLinkList))
-print(LinkFileList[0])
-
-
-#Read 1-DimensionFeature CSV file
-df1 = pd.read_csv("/Users/admin/PycharmProjects/fileProcessing/1-DimensionFinalNew1originSub.csv", low_memory=False)
-df2 = pd.DataFrame(LinkFileList)
-df2.columns = ["FileName","ImgLink"]
-# df1['FileName']=df1['FileName'].astype(int)
-# df2['FileName']=df1['FileName'].astype(int)
-
-#Merge LinkList and FileInfoList with 1-Dimension feature
-df3 = pd.merge(df1, df2, how='inner', on=['FileName'], sort='Group')
-df4 = df3.drop_duplicates()
-df4.to_csv("ImageInfo.csv",index=False)
-
 
 #Download the images to each subsets
 def createSubsetCategoriesList(categories,subsets):
