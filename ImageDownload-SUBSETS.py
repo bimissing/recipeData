a2460cd55a7df6771b77f0327efdcb9ca1933756
bimissing <39038771+bimissing@users.noreply.github.com>
2021-04-18 01:47:33 +0900

    Update ImageDownload-SUBSETS.py

--- ImageDownload-SUBSETS.py
+++ ImageDownload-SUBSETS.py
@@ -13,7 +13,7 @@ from shutil import copy2
 
 
 #Download the images to each subsets
-def createSubsetCategoriesList(categories,subsets):
+def create_subset_categories_list(categories,subsets):
     with open('ImageInfo.csv', 'r') as csvFile:
         reader = csv.reader(csvFile)
         result = [row for row in reader]
@@ -27,16 +27,16 @@ def createSubsetCategoriesList(categories,subsets):
             # print(n)
             for i in range(1, len(result)):
                 if result[i][5] == str(n) and result[i][6] == str(m):
-                    imageLink = result[i][7]
+                    image_link = result[i][7]
                     # print(imageLink)
                     # count = count + 1
-                    imgpath = "./subsets/"+str(m)+"/"+str(n)+"/"
-                    if not os.path.exists(imgpath):
+                    img_path = "./subsets/"+str(m)+"/"+str(n)+"/"
+                    if not os.path.exists(img_path):
                         # os.mkdir(imgpath)
-                        os.makedirs(imgpath)
+                        os.makedirs(img_path)
                     try:
-                        response = requests.get(imageLink)
-                        file = open(imgpath+result[i][2]+".jpg", "wb")
+                        response = requests.get(image_link)
+                        file = open(img_path+result[i][2]+".jpg", "wb")
                         file.write(response.content)
                         file.close()
                     except:
@@ -45,4 +45,5 @@ def createSubsetCategoriesList(categories,subsets):
 
 subsets=[1,2,3]
 categories=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-createSubsetCategoriesList(categories,subsets)
+# 1 Potato, 2 ginger, 3 onion, 4 pork, 5 shrimp, 6 chicken, 7 corn, 8 carrot, 9 eggplant, 10 shallot, 11 tofu, 12 spinach, 13 sauce, 14 chili, 15bread, 16 dough, 17 fish, 18 egg, 19 cucumber and 20 soybean.
+create_subset_categories_list(categories,subsets)
